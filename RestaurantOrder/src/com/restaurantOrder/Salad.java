9c4c3a8bd2834ff1f1c46b27fda83a1df6f1961f
scode234 <scodes3451@gmail.com>
2023-01-15 17:28:19 -0600

    Add variables, getters, and setters

--- RestaurantOrder/src/com/restaurantOrder/Salad.java
+++ RestaurantOrder/src/com/restaurantOrder/Salad.java
@@ -1,5 +1,20 @@
 package com.restaurantOrder;
 
 public class Salad extends MenuItem {
-	 
+	
+	private String flavor;
+	private String dressing;
+	
+	public String getFlavor() {
+		return flavor;
+	}
+	public void setFlavor(String flavor) {
+		this.flavor = flavor;
+	}
+	public String getDressing() {
+		return dressing;
+	}
+	public void setDressing(String dressing) {
+		this.dressing = dressing;
+	}
 }
