1a13e3ec7784d3ffb789d09460a754f60cd6434a
scode234 <scodes3451@gmail.com>
2023-01-15 21:11:54 -0600

    Add test for order number

--- RestaurantOrder/src/com/restaurantOrder/tests/Tests.java
+++ RestaurantOrder/src/com/restaurantOrder/tests/Tests.java
@@ -2,13 +2,20 @@ package com.restaurantOrder.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
+
+import com.restaurantOrder.Order;
+
 import org.junit.jupiter.api.Test;
 
 class Tests {
+	
+	private final Order order1 = new Order();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void doesOrderNumberGenerate() {
+		int orderNumber = order1.getOrderNumber();
+		Assert.assertNotEquals(0, orderNumber);
 	}
 
 }
