f0b30c5f4d79e547859d3501492f75a93c20d5f7
scode234 <scodes3451@gmail.com>
2023-01-15 20:38:06 -0600

    Initial commit

--- RestaurantOrder/src/com/restaurantOrder/Main.java
+++ RestaurantOrder/src/com/restaurantOrder/Main.java
@@ -1,10 +1,13 @@
 package com.restaurantOrder;
 
+import java.util.Scanner;
+
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
-		
+		System.out.println("Welcome to the restaurant! Please follow the prompts to place your order.");
+		Order order = new Order();
+
 	}
 
 }
