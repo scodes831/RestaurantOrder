d3018b75d225133ba4918e14af9f64d4140fa908
scode234 <scodes3451@gmail.com>
2023-01-15 19:04:58 -0600

    Add askOrderType method

--- RestaurantOrder/src/com/restaurantOrder/UserPrompts.java
+++ RestaurantOrder/src/com/restaurantOrder/UserPrompts.java
@@ -1,5 +1,19 @@
 package com.restaurantOrder;
 
+import java.util.Scanner;
+
 public class UserPrompts {
 
+	public static void askOrderType(Order order) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please select the order type:\n1 - Dine In\n2 - Takeout");
+		int selection = in.nextInt();
+		if (selection == 1) {
+			order.setOrderType("Dine In");
+		} else  {
+			order.setOrderType("Takeout");
+		}
+	}
+	
+	
 }
