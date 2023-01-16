e99437b208019c67672bee8fd84e6e70c73dfeec
scode234 <scodes3451@gmail.com>
2023-01-16 17:46:03 -0600

    Add single scanner and pass to methods called

--- RestaurantOrder/src/com/restaurantOrder/Main.java
+++ RestaurantOrder/src/com/restaurantOrder/Main.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("Welcome to the restaurant! Please follow the prompts to place your order.");
+		Scanner in = new Scanner(System.in);
 		Order order = new Order();
+		UserPrompts.askOrderType(order, in);
+		UserPrompts.askOrderItems(order, in);
 
 	}
 
