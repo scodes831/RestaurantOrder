9d7d55af11d2817339da96fb45a8e8bbc79c5561
scode234 <scodes3451@gmail.com>
2023-01-15 18:48:29 -0600

    Add variables, getters, and setters

--- RestaurantOrder/src/com/restaurantOrder/Pizza.java
+++ RestaurantOrder/src/com/restaurantOrder/Pizza.java
@@ -2,5 +2,27 @@ package com.restaurantOrder;
 
 public class Pizza extends MenuItem {
 
+	private String size;
+	private String flavor;
+	private String crust;
+	
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getFlavor() {
+		return flavor;
+	}
+	public void setFlavor(String flavor) {
+		this.flavor = flavor;
+	}
+	public String getCrust() {
+		return crust;
+	}
+	public void setCrust(String crust) {
+		this.crust = crust;
+	}
 
 }
