4daa2ced435367e63ea25225fa3bce31889c8ac1
scode234 <scodes3451@gmail.com>
2023-01-16 14:18:13 -0600

    Add show methods to prompt user for menu item selections

--- RestaurantOrder/src/com/restaurantOrder/UserPrompts.java
+++ RestaurantOrder/src/com/restaurantOrder/UserPrompts.java
@@ -13,6 +13,25 @@ public class UserPrompts {
 		} else {
 			order.setOrderType("Takeout");
 		}
+		in.close();
+	}
+	
+	public static void askOrderItems(Order order) throws Exception {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please select an item:\n1 - Pizza.....$8\n2 - Burger....$10\n3 - Salad.....$7");
+		int selection = in.nextInt();
+		if (selection == 1) {
+			Pizza pizza = new Pizza();
+			pizza.showPizzaOptions(pizza);
+		} else if (selection == 2) {
+			Burger burger = new Burger();
+			burger.showBurgerOptions(burger);
+		} else if (selection == 3) {
+			Salad salad = new Salad();
+			salad.showSaladOptions(salad);
+		} else {
+			throw new Exception("Please choose a valid selection.");
+		}
 	}
 
 }
