ee73f7a26322cf70bd479f8d0dcb800917e9a13d
scode234 <scodes3451@gmail.com>
2023-01-16 17:46:20 -0600

    Remove separate instances of scanner and add as parameter instead

--- RestaurantOrder/src/com/restaurantOrder/UserPrompts.java
+++ RestaurantOrder/src/com/restaurantOrder/UserPrompts.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class UserPrompts {
 
-	public static void askOrderType(Order order) {
-		Scanner in = new Scanner(System.in);
+	public static void askOrderType(Order order, Scanner in) {
 		System.out.println("Please select the order type:\n1 - Dine In\n2 - Takeout");
 		int selection = in.nextInt();
 		if (selection == 1) {
@@ -13,25 +12,30 @@ public class UserPrompts {
 		} else {
 			order.setOrderType("Takeout");
 		}
-		in.close();
 	}
-	
-	public static void askOrderItems(Order order) throws Exception {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Please select an item:\n1 - Pizza.....$8\n2 - Burger....$10\n3 - Salad.....$7");
-		int selection = in.nextInt();
-		if (selection == 1) {
-			Pizza pizza = new Pizza();
-			pizza.showPizzaOptions(pizza);
-		} else if (selection == 2) {
-			Burger burger = new Burger();
-			burger.showBurgerOptions(burger);
-		} else if (selection == 3) {
-			Salad salad = new Salad();
-			salad.showSaladOptions(salad);
-		} else {
-			throw new Exception("Please choose a valid selection.");
-		}
+
+	public static void askOrderItems(Order order, Scanner in) throws Exception {
+		boolean selectionError = false;
+		do {
+			System.out.println("Please select an item (press 0 when finished):\n1 - Pizza.....$8\n2 - Burger....$10\n3 - Salad.....$7");
+			int selection = in.nextInt();
+			if (selection == 1) {
+				Pizza pizza = new Pizza();
+				pizza.showPizzaOptions(pizza, in);
+			} else if (selection == 2) {
+				Burger burger = new Burger();
+				burger.showBurgerOptions(burger, in);
+			} else if (selection == 3) {
+				Salad salad = new Salad();
+				salad.showSaladOptions(salad, in);
+			} else if (selection == 0) {
+				System.out.println("Items have been added.");
+			} else {
+				selectionError = true;
+				throw new Exception("Please choose a valid selection.");
+			}
+
+		} while (selectionError);
 	}
 
 }
