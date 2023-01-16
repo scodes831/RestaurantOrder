b57c02f0c521328ee1d4d1ff5475a6b71e263b98
scode234 <scodes3451@gmail.com>
2023-01-15 20:37:38 -0600

    Add variables, getters, and setters

--- RestaurantOrder/src/com/restaurantOrder/Burger.java
+++ RestaurantOrder/src/com/restaurantOrder/Burger.java
@@ -3,30 +3,33 @@ package com.restaurantOrder;
 import java.util.ArrayList;
 
 public class Burger extends MenuItem {
-	
+
 	private String cookingPreference;
 	private ArrayList<String> toppings;
 	private boolean hasBun;
-	
+
 	public String getCookingPreference() {
 		return cookingPreference;
 	}
+
 	public void setCookingPreference(String cookingPreference) {
 		this.cookingPreference = cookingPreference;
 	}
+
 	public ArrayList<String> getToppings() {
 		return toppings;
 	}
+
 	public void setToppings(ArrayList<String> toppings) {
 		this.toppings = toppings;
 	}
+
 	public boolean isHasBun() {
 		return hasBun;
 	}
+
 	public void setHasBun(boolean hasBun) {
 		this.hasBun = hasBun;
 	}
-	
-	
 
 }
