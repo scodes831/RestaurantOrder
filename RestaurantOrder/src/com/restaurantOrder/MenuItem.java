ab30e5a43555491aeea00422d0a5e3d919566d05
scode234 <scodes3451@gmail.com>
2023-01-15 20:38:20 -0600

    Add variables, getters, and setters

--- RestaurantOrder/src/com/restaurantOrder/MenuItem.java
+++ RestaurantOrder/src/com/restaurantOrder/MenuItem.java
@@ -3,26 +3,31 @@ package com.restaurantOrder;
 import java.math.BigDecimal;
 
 public abstract class MenuItem {
-	
+
 	private BigDecimal price;
 	private String description;
 	private String specialInstructions;
-	
+
 	public BigDecimal getPrice() {
 		return price;
 	}
+
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getSpecialInstructions() {
 		return specialInstructions;
 	}
+
 	public void setSpecialInstructions(String specialInstructions) {
 		this.specialInstructions = specialInstructions;
 	}
