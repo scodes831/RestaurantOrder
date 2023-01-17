859f46b88acaeef4ecc66b9a54e53569ceee70c9
scode234 <scodes3451@gmail.com>
2023-01-16 19:02:18 -0600

    Add confirmPizza method to show user the pizza they ordered after they have chosen all selections

--- RestaurantOrder/src/com/restaurantOrder/Pizza.java
+++ RestaurantOrder/src/com/restaurantOrder/Pizza.java
@@ -43,29 +43,57 @@ public class Pizza extends MenuItem {
 	}
 
 	private void choosePizzaToppings(Pizza pizza, Scanner in) throws Exception {
-		
-		System.out.println(
-				"Select toppings: (press 0 when finished)\n1 - Cheese\n2 - Pepperoni (+$2)\n3 - Sausage (+$2)\n4 - Chicken (+$2)\n5 - Bell Peppers"
-						+ "\n6 - Olives\n7 - Extra Cheese (+$1)");
-		int selection = in.nextInt();
-		if (selection == 1) {
-			pizza.getToppings().add("Cheese");
-		} else if (selection == 2) {
-			pizza.getToppings().add("Pepperoni");
-		} else if (selection == 3) {
-			pizza.getToppings().add("Sausage");
-		} else if (selection == 4) {
-			pizza.getToppings().add("Chicken");
-		} else if (selection == 5) {
-			pizza.getToppings().add("Bell Peppers");
-		} else if (selection == 6) {
-			pizza.getToppings().add("Olives");
-		} else if (selection == 7) {
-			pizza.getToppings().add("Extra Cheese");
-		} else {
-			throw new Exception("Please enter a valid selection.");
+		int selection;
+		do {
+			System.out.println(
+					"Select toppings: (press 0 when finished)\n1 - Cheese\n2 - Pepperoni (+$2)\n3 - Sausage (+$2)\n4 - Chicken (+$2)\n5 - Bell Peppers"
+							+ "\n6 - Olives\n7 - Extra Cheese (+$1)");
+			selection = in.nextInt();
+			if (selection == 1) {
+				pizza.getToppings().add("Cheese");
+			} else if (selection == 2) {
+				pizza.getToppings().add("Pepperoni");
+			} else if (selection == 3) {
+				pizza.getToppings().add("Sausage");
+			} else if (selection == 4) {
+				pizza.getToppings().add("Chicken");
+			} else if (selection == 5) {
+				pizza.getToppings().add("Bell Peppers");
+			} else if (selection == 6) {
+				pizza.getToppings().add("Olives");
+			} else if (selection == 7) {
+				pizza.getToppings().add("Extra Cheese");
+			} else if (selection == 0) {
+				System.out.println("Toppings have been added.");
+			} else {
+				throw new Exception("Please enter a valid selection.");
+			}
+		} while (selection != 0);
+
+		confirmPizza(pizza);
+	}
+
+	public void confirmPizza(Pizza pizza) {
+		StringBuilder toppingsStr = new StringBuilder();
+
+		for (int i = 0; i < pizza.getToppings().size(); i++) {
+			toppingsStr.append(pizza.getToppings().get(i).toLowerCase());
+			
+			if (pizza.getToppings().size() == 1) {
+				continue;
+			} else if (pizza.getToppings().size() == 2 && i == 0) {
+				toppingsStr.append(" and ");
+			} else if (pizza.getToppings().size() == 2 && i == 1) {
+				continue;
+			} else if (i == pizza.getToppings().size() - 2) {
+				toppingsStr.append(", and ");
+			} else if (i != pizza.getToppings().size() - 1) {
+				toppingsStr.append(", ");
+			}
 		}
 
+		System.out.println("You have added a " + pizza.getSize().toLowerCase() + " " + pizza.getCrust().toLowerCase()
+				+ " crust pizza with " + toppingsStr.toString() + " to your order");
 	}
 
 	public String getSize() {
