3465fd88873c48997d25fd8c7421fcb386ebc176
scode234 <scodes3451@gmail.com>
2023-01-18 18:17:36 -0600

    Add tests for calculateSaladPrice and calculateBurgerPrice methods

--- RestaurantOrder/src/com/restaurantOrder/tests/Tests.java
+++ RestaurantOrder/src/com/restaurantOrder/tests/Tests.java
@@ -5,8 +5,10 @@ import java.math.BigDecimal;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import com.restaurantOrder.Burger;
 import com.restaurantOrder.Order;
 import com.restaurantOrder.Pizza;
+import com.restaurantOrder.Salad;
 
 class Tests {
 	
@@ -45,5 +47,37 @@ class Tests {
 		pizza3.getToppings().add("Extra Cheese");
 		pizza3.calculatePizzaPrice(pizza3);
 		Assert.assertEquals(new BigDecimal(13), pizza3.getPrice());
+	}
+	
+	@Test
+	void doesFetaCheeseSaladCost7() {
+		Salad salad1 = new Salad();
+		salad1.getToppings().add("Feta Cheese");
+		salad1.calculateSaladPrice(salad1);
+		Assert.assertEquals(new BigDecimal(7), salad1.getPrice());		
+	}
+	
+	@Test
+	void doesCheddarCheeseSaladCost7() {
+		Salad salad2 = new Salad();
+		salad2.getToppings().add("Cheddar Cheese");
+		salad2.calculateSaladPrice(salad2);
+		Assert.assertEquals(new BigDecimal(7), salad2.getPrice());		
+	}
+	
+	@Test
+	void doesAmericanCheeseBurgerCost7() {
+		Burger burger1 = new Burger();
+		burger1.getToppings().add("Ketchup");
+		burger1.getToppings().add("American Cheese");
+		burger1.calculateBurgerPrice(burger1);
+		Assert.assertEquals(new BigDecimal(7), burger1.getPrice());		
+	}
+	
+	@Test
+	void doesPlainBurgerCost6() {
+		Burger burger2 = new Burger();
+		burger2.calculateBurgerPrice(burger2);
+		Assert.assertEquals(new BigDecimal(6), burger2.getPrice());		
 	}
  }
