ac3e29a33ac5e2e6e53acd8dd770f82fbf7d4385
scode234 <scodes3451@gmail.com>
2023-01-18 18:02:50 -0600

    Add 3 tests for calculatePizzaPrice method

--- RestaurantOrder/src/com/restaurantOrder/tests/Tests.java
+++ RestaurantOrder/src/com/restaurantOrder/tests/Tests.java
@@ -1,21 +1,49 @@
 package com.restaurantOrder.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 import com.restaurantOrder.Order;
-
-import org.junit.jupiter.api.Test;
+import com.restaurantOrder.Pizza;
 
 class Tests {
 	
-	private final Order order1 = new Order();
-
 	@Test
 	void doesOrderNumberGenerate() {
-		int orderNumber = order1.getOrderNumber();
+		Order order = new Order();
+		int orderNumber = order.getOrderNumber();
 		Assert.assertNotEquals(0, orderNumber);
 	}
-
-}
+	
+	@Test
+	void doesSmallCheesePizzaCost5() {
+		Pizza pizza1 = new Pizza();
+		pizza1.setSize("Small");
+		pizza1.getToppings().add("Cheese");
+		pizza1.calculatePizzaPrice(pizza1);
+		System.out.println(pizza1.getPrice());
+		Assert.assertEquals(new BigDecimal(5), pizza1.getPrice());
+	}
+	
+	@Test
+	void doesMediumSausageChickenPizzaCost11() {
+		Pizza pizza2 = new Pizza();
+		pizza2.setSize("Medium");
+		pizza2.getToppings().add("Sausage");
+		pizza2.getToppings().add("Chicken");
+		pizza2.calculatePizzaPrice(pizza2);
+		Assert.assertEquals(new BigDecimal(11), pizza2.getPrice());
+	}
+	
+	@Test
+	void doesLargePepperoniExtraCheesePizzaCost13() {
+		Pizza pizza3 = new Pizza();
+		pizza3.setSize("Large");
+		pizza3.getToppings().add("Pepperoni");
+		pizza3.getToppings().add("Extra Cheese");
+		pizza3.calculatePizzaPrice(pizza3);
+		Assert.assertEquals(new BigDecimal(13), pizza3.getPrice());
+	}
+ }
