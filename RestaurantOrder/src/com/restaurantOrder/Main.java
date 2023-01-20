ada00000d4ce7ad7a6e121c5eec4eba3b7f1cb3b
scode234 <scodes3451@gmail.com>
2023-01-19 18:57:52 -0600

    Call methods

--- RestaurantOrder/src/com/restaurantOrder/Main.java
+++ RestaurantOrder/src/com/restaurantOrder/Main.java
@@ -1,5 +1,7 @@
 package com.restaurantOrder;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +12,10 @@ public class Main {
 		Order order = new Order();
 		UserPrompts.askOrderType(order, in);
 		UserPrompts.askOrderItems(order, in);
-
+		order.calculateOrderSubTotal(order);
+		UserPrompts.askForTip(order, in);
+		order.calculateOrderTotal(order);
+		UserPrompts.askToViewReceipt(order, in);
 	}
 
 }
